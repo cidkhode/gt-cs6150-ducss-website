351ff07b4af98f1130c768747a79abf4070a3a3d
cidkhode <chidanandkhode@gmail.com>
2024-04-28 11:13:26 -0400

    upd links an weekly upd

--- src/project-info.jsx
+++ src/project-info.jsx
@@ -55,7 +55,9 @@ export const ProjectInfo = () => {
             <li><a href="https://youtu.be/1Xv03rUTV-4?si=0fa982ZFE1z-QuBh" target="_blank" rel="noreferrer"><Typography color="#66A3F0" sx={{ height: 50 }}>DEMO VIDEO</Typography></a></li>
             <li><a href="https://docs.google.com/presentation/d/1e8eGTrQf6HoqbooUOgO129QQm_RiaR-hvaaUIfPYREs/edit?usp=sharing" target="_blank" rel="noreferrer"><Typography color="#66A3F0" sx={{ height: 50 }}>PRESENTATION SLIDES</Typography></a></li>
             <li><a href="https://docs.google.com/document/d/17OCxllCIsq8DLwdlY5mLHt6PFu_ZsfzrL3y76MPJcrA/edit?usp=sharing" target="_blank" rel="noreferrer"><Typography color="#66A3F0" sx={{ height: 50 }}>APK INSTALLATION INSTRUCTIONS</Typography></a></li>
-            <li><a href="https://drive.google.com/file/d/13TLc8n4OQDZhoCiBTEp2YdbC8OXZbHQy/view?usp=drive_link" target="_blank" rel="noreferrer"><Typography color="#66A3F0" sx={{ height: 50 }}>APK(GOOGLE DRIVE)</Typography></a></li>                 
+            <li><a href="https://drive.google.com/file/d/13TLc8n4OQDZhoCiBTEp2YdbC8OXZbHQy/view?usp=drive_link" target="_blank" rel="noreferrer"><Typography color="#66A3F0" sx={{ height: 50 }}>APK(GOOGLE DRIVE)</Typography></a></li>
+            <li><a href="https://docs.google.com/document/d/1ZnjSX_bIyDE27oLJK7YGRDAKirETrw7V7W_5zVqCaFg" target="_blank" rel="noreferrer"><Typography color="#66A3F0" sx={{ height: 50 }}>DEVELOPER DOCUMENTATION</Typography></a></li>
+            <li><a href="https://github.gatech.edu/dstrube3/CS6150-DUCSS" target="_blank" rel="noreferrer"><Typography color="#66A3F0" sx={{ height: 50 }}>GITHUB FOR ANDROID APP</Typography></a></li>
           </ul>
         </Typography>
       </Box>
@@ -64,14 +66,12 @@ export const ProjectInfo = () => {
       <Typography color="white" textAlign="center">WEEKLY TEAM UPDATE</Typography>
       <Box sx={{ margin: "8px 8px 16px", justifyContent: "center", display: "flex", padding: 2, border: "solid 2px green", borderRadius: 5, "> a": { textDecorationColor: "#66A3F0" }, ">:first-of-type": { marginRight: 4 } }}>
       <Typography component="div" color="white">
-          This week our team is working towards:
+          This week (4/21 thru 4/28) our team is working towards:
           <ul>
-            <li><s>CSV Data Export</s></li>
-            <li><s>Step counter first implementation</s></li>
-            <li><s>Home screen</s></li>
-            <li><s>Addressing feedback received in surveys</s></li>
-            <li><s>Template visualization comparing most recent collection against healthy baseline</s></li>
-            <li>First cut at Suggested Notes for Doctor's Visit</li>
+            <li>Preparing the final report</li>
+            <li>Tidying up loose ends in app</li>
+            <li>Analyzing some user feedback we received from our project stakeholders</li>
+            <li>Coordinate one final meeting with staholders and prepare project hand-off</li>
           </ul>
         </Typography>
       </Box>
